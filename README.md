0badc9cfefa125d946a360acecf682c8a58b2447
Kevin Johnson <kevinejohn@gmail.com>
2018-10-17 15:24:25 +0200

    Added docker install script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # How to use
 
 ```
+./install_docker.sh
+
 docker-compose up -d
 ```
---
+++ install_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-docker-ce
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install docker-ce
+
+
+# Install docker-compose
+# https://docs.docker.com/compose/install/#install-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "Installed Docker"
